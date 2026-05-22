4dd3144b4aad57b6b3545a1cd01de32cf8e54e23
virpuragirirajsinh-a11y <virpuragirirajsinh@gmail.com>
2026-05-22 13:13:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -300,6 +300,16 @@ message:
 }
 
 });
-app.listen(5000, () => {
-    console.log("Server Running...");
+const PORT =
+process.env.PORT
+|| 5000;
+
+app.listen(
+PORT,
+() => {
+
+console.log(
+"Server Running 😎"
+);
+
 });
